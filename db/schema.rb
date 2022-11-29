128f60b134d8e873eda6b604a0e413dceb486d34
Rodolfo Campos <rodolfo123@gmail.com>
2022-11-29 17:18:46 -0300

    updated schema with remote

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_172106) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_190613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_172106) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "remote"
     t.index ["teacher_id"], name: "index_appointments_on_teacher_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
@@ -52,13 +53,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_172106) do
     t.boolean "friday"
     t.boolean "saturday"
     t.boolean "sunday"
-    t.time "start_time"
-    t.time "end_time"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.integer "price"
     t.boolean "remote"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "in_person"
     t.index ["user_id"], name: "index_teachers_on_user_id"
   end
 
