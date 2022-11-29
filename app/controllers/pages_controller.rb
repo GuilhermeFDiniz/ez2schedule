3faa3b59a232943537b46147a8430f567db0a3b2
Rodolfo Campos <rodolfo123@gmail.com>
2022-11-29 15:18:49 -0300

    hotfixes after schema change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @slots = Slot.all
+    @appointments = Appointment.all
   end
 end
