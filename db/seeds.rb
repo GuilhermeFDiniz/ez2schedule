80917b2f40a1b11620d830d95b80726809aaf77f
Rodrigo Adilson <rodrigo.mrtns@hotmail.com>
2022-11-29 11:50:59 -0300

    Seed de Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!(email: "gui@lewagon", password: "123123")
-User.create!(email: "student@lewagon", password: "123123")
-
-Slot.create!(user: User.first, start_time: Time.now, end_time: (Time.now + 1))
+Category.create!(name: 'HTML', image: 'https://www.shecodes.io/assets/logos/technologies/html-0604a6b4c136a971d7ea5d7f5941216ae895093c636027fc3ac53297fea9a502.png')
+Category.create!(name: 'CSS', image: 'https://www.shecodes.io/assets/logos/technologies/css-c4554008372faa24096997403ee46f3347a87b8f1636cfb148109bac867c03bb.png')
+Category.create!(name: 'JavaScript', image: 'https://www.shecodes.io/assets/logos/technologies/javascript-66f17d0db91bf422711333a5899e3ce6e952071c8c7b747021029a41fbf42cd7.png')
+Category.create!(name: 'Ruby', image: 'https://www.ruby-lang.org/images/header-ruby-logo.png')
+Category.create!(name: 'Python', image: 'https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png')
+Category.create!(name: 'PHP', image: 'https://www.php.net//images/logos/new-php-logo.svg')
+Category.create!(name: 'React', image: 'https://www.shecodes.io/assets/logos/technologies/react-600b92c82a6b987573201ac95a30d8fcd6789a803c473337aadb2c2865cb7919.png')
+Category.create!(name: 'Node.js', image: 'https://logospng.org/wp-content/uploads/node-js.png')
+Category.create!(name: 'JAVA', image: 'https://static.javatpoint.com/core/images/java-logo3.png')
+Category.create!(name: 'C', image: 'https://www.britefish.net/wp-content/uploads/2019/07/logo-c-1.png')
