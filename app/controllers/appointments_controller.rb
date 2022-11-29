ff67e1d2d28e9ea03ea4627ab0ab767010f89a52
Rodrigo Adilson <rodrigo.mrtns@hotmail.com>
2022-11-29 16:01:23 -0300

    Ainda fazendo create de teachers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,6 @@
 class AppointmentsController < ApplicationController
-
 def show
 
 end
-
 
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,17 @@
 class TeachersController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
