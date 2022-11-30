28b2dcd57eb52b2a3e9a7d12f92628c070952c9c
Guilherme Diniz <guidinizeng@gmail.com>
2022-11-30 18:35:52 -0300

    calendar

--- app/javascript/controllers/teacher_calendar_controller.js
+++ app/javascript/controllers/teacher_calendar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="teacher-calendar"
 export default class extends Controller {
-  static targets = ["mark"]
+  static targets = ["mark", "start", "end"]
   connect() {
     console.log("oi")
   }
@@ -12,11 +12,14 @@ export default class extends Controller {
     this.markTargets.forEach((cell)=> {
       cell.classList.remove("bg-danger")
     });
-
     if (event.currentTarget.innerText === "-"){
       return
     } else {
       event.currentTarget.classList.add("bg-danger")
+      console.log(this.startTarget.value)
+      console.log(this.endTarget.value)
+      this.startTarget.value = event.currentTarget.innerText
+      this.endTarget.value = `${(parseInt(event.currentTarget.innerText) + 1)}:00`
     }
   }
 }
