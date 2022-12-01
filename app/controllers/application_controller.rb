8bb68b0b47fbf672067d94a119875e18d340f180
Rodrigo Adilson <rodrigo.mrtns@hotmail.com>
2022-12-01 16:12:37 -0300

    Foto de avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   include Pundit::Authorization
 
@@ -21,9 +22,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :about])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :about, :avatar_image])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :about])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :about, :avatar_image])
   end
 end
