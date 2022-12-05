4cd62b0e44b65a642e61b37920bf851623d2782a
Rodolfo Campos <rodolfo123@gmail.com>
2022-12-05 14:55:32 -0300

    implemented cat bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,13 @@ class PagesController < ApplicationController
     @teachers_ruby = Teacher.joins(:categories).where(categories: { name: 'Ruby' })
     @teachers_html = Teacher.joins(:categories).where(categories: { name: 'HTML' })
     @teachers_css = Teacher.joins(:categories).where(categories: { name: 'CSS' })
+    @categories = Category.all
 
     if params[:query].present?
       @teachers = Teacher.search_by_all(params[:query])
+    end
+    if params[:filter].present?
+      @teachers = Teacher.search_by_all(params[:filter])
     end
   end
 end
