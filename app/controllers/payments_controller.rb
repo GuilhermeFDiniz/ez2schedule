87c88459c4e650999bb1ab7dc62323b83788fff7
Rodrigo Adilson <rodrigo.mrtns@hotmail.com>
2022-12-07 15:17:36 -0300

    Página de confirmação

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,4 +3,8 @@ class PaymentsController < ApplicationController
     @appointment = current_user.appointments.where(state: 'pending').find(params[:appointment_id])
   authorize @appointment
   end
+
+  def show
+    @payment = Payment.find(params(:id))
+  end
 end
