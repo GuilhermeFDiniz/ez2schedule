dada20ca7736a488a45686129a8ed3285ee28901
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-08 13:42:53 -0300

    Show Appointment

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < ApplicationRecord
   belongs_to :user
-  has_many :appointments
-  has_many :teacher_categories
+  has_many :appointments, dependent: :destroy
+  has_many :teacher_categories, dependent: :destroy
   has_many :categories, through: :teacher_categories
   has_one_attached :teacher_video
   monetize :price_cents
